fd7f3a01bee35b4b65ded2484ef794ded346b1e0
Nikolay Zalutskiy <pacemkr@gmail.com>
2013-12-01 00:47:25 -0500

    Naive implementation.

--- src/App.scala
+++ src/App.scala
@@ -1,40 +1,46 @@
-import org.keyczar.{Crypter}
+import org.keyczar.Crypter
 import scala.io.{BufferedSource, Source}
-import scala.pickling._
-import json._
-import scala.io._
+import spray.json._
+import DefaultJsonProtocol._
 import scala.reflect.io.{Path, File}
+import org.apache.log4j
 
 object App {
-  case class Entry(title:String, password:String)
+  log4j.Logger.getRootLogger.setLevel(log4j.Level.OFF)
+
   type Db = List[Entry]
+  case class Entry(title:String, payload:List[(String, String)])
 
-  val fp = "test.encrypted"
-  val crypter = new Crypter("/Volumes/Snappy/.keyczar")
-
-  def encrypt(db:Db) = {
-    val pickled = db.pickle
-    val cyphertext = crypter.encrypt(pickled.value)
-
-    Path(fp).toFile.writeAll(cyphertext)
+  object EntryJsonProtocol extends DefaultJsonProtocol {
+    implicit val entryFormat = jsonFormat2(Entry)
   }
+  import EntryJsonProtocol._
 
+  val fp = "/Users/nick/my.secrets"
+  val crypter = new Crypter("/Users/nick/.keyczar")
+
+  def encrypt(text:String) = crypter.encrypt(text)
   def decrypt(cyphertext:String) = crypter.decrypt(cyphertext)
   def read = File(fp).slurp
+  def write(cyphertext:String) = Path(fp).toFile.writeAll(cyphertext)
+  def serialize(db:Db) = db.toJson.toString
+  def deserialize(json:String) = json.asJson.convertTo[Db]
+  def save(db:Db) = write(encrypt(serialize(db)))
+  def load:Db = deserialize(decrypt(read))
 
   def list = {
-    val db:List[Int] = decrypt(read).unpickle[List[Int]]
-    println(db)
-    //val db:Db = decrypt(read("test.encrypted"))
+    val db:Db = load
+
+    db.foreach(e => println(s"${e.title}\t${e.payload(0)._2}"))
   }
 
   def importFromSource(source:BufferedSource) = {
     val entryMatch = """(^[^\[].*)[\s](.*)""".r
     val db:Db = source.getLines.collect {
-      case entryMatch(title, password) => Entry(title, password)
+      case entryMatch(title, password) => Entry(title, List(("password", password)))
     }.toList
 
-    encrypt(db)
+    save(db)
   }
 
   def main(args:Array[String]) = args match {
